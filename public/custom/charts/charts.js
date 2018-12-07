57f318aa37e322bc867376b3c68bff99a22b9677
fql <fql@fqldeMacBook-Air.local>
2018-12-07 15:16:08 +0800

    charts resize automatically

--- public/custom/charts/charts.js
+++ public/custom/charts/charts.js
@@ -178,6 +178,14 @@ function abnormalVisual(abnomalData) {
 
     // 使用刚指定的配置项和数据显示图表。
     myChart.setOption(option);
+    $("#abnormalChart").resize(function () {
+        myChart.resize();
+    });
+
+    window.onresize = function () {
+        ///用于使chart自适应高度和宽度
+        myChart.resize();
+    };
 }
 
 function motifVisual(motifData)
@@ -321,6 +329,14 @@ function motifVisual(motifData)
 
 
     myChart.setOption(option);
+    $("#motifChart").resize(function () {
+        myChart.resize();
+    });
+
+    window.onresize = function () {
+        ///用于使chart自适应高度和宽度
+        myChart.resize();
+    };
 }
 
 
@@ -383,3 +399,81 @@ function showGraph(data) {
     //用于使chart自适应高度和宽度
     window.onresize = myChart.resize();
 }
+
+
+//监听div大小变化
+//之后剥离到其他util文件中去
+(function($, h, c) {
+    var a = $([]),
+        e = $.resize = $.extend($.resize, {}),
+        i,
+        k = "setTimeout",
+        j = "resize",
+        d = j + "-special-event",
+        b = "delay",
+        f = "throttleWindow";
+    e[b] = 250;
+    e[f] = true;
+    $.event.special[j] = {
+        setup: function() {
+            if (!e[f] && this[k]) {
+                return false;
+            }
+            var l = $(this);
+            a = a.add(l);
+            $.data(this, d, {
+                w: l.width(),
+                h: l.height()
+            });
+            if (a.length === 1) {
+                g();
+            }
+        },
+        teardown: function() {
+            if (!e[f] && this[k]) {
+                return false;
+            }
+            var l = $(this);
+            a = a.not(l);
+            l.removeData(d);
+            if (!a.length) {
+                clearTimeout(i);
+            }
+        },
+        add: function(l) {
+            if (!e[f] && this[k]) {
+                return false;
+            }
+            var n;
+            function m(s, o, p) {
+                var q = $(this),
+                    r = $.data(this, d);
+                r.w = o !== c ? o: q.width();
+                r.h = p !== c ? p: q.height();
+                n.apply(this, arguments);
+            }
+            if ($.isFunction(l)) {
+                n = l;
+                return m;
+            } else {
+                n = l.handler;
+                l.handler = m;
+            }
+        }
+    };
+    function g() {
+        i = h[k](function() {
+                a.each(function() {
+                    var n = $(this),
+                        m = n.width(),
+                        l = n.height(),
+                        o = $.data(this, d);
+                    if (m !== o.w || l !== o.h) {
+                        n.trigger(j, [o.w = m, o.h = l]);
+                    }
+                });
+                g();
+            },
+            e[b]);
+    }
+})(jQuery, this);
