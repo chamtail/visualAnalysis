17d9b2a4ead84b29d6f29efd89f5eb95f45c4d76
fql <fql@fqldeMacBook-Air.local>
2019-01-19 20:33:32 +0800

    add matrixProfile visual part

--- public/custom/node/node.js
+++ public/custom/node/node.js
@@ -788,9 +788,8 @@ function visualize(data) {
             break;
         case "hime":
         case "matrix-profile":
-            $('#motifArea').attr('style', 'display:block');
-            var motifData = generateMotifData(5);
-            motifAreaVisual(motifData);
+            $('#matrixProfile').attr('style', 'display:block');
+            matrixProfileVisual("matrixProfile",null);
             break;
         case "cluster":
         case "seql":
